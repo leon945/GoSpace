4ea39ae93095e3c24abf5dd7de92f7be14e02b16
Leon de la Garza <leonjavier945@gmail.com>
2022-11-30 22:46:16 -0600

    Added keyboard input

--- main.go
+++ main.go
@@ -43,35 +43,11 @@ func update(window draw.Window) {
 	}
 
 	runGGOUpdates()
+	handleMouse(window)
+	handleKeyboard(window)
+}
 
-	// // find the screen center
-	// w, h := window.Size()
-	// centerX, centerY := w/2, h/2
-
-	// // draw a button in the center of the screen
-	// mouseX, mouseY := window.MousePosition()
-	// mouseInCircle := math.Hypot(float64(mouseX-centerX), float64(mouseY-centerY)) < 20
-	// color := draw.DarkRed
-	// if mouseInCircle {
-	// 	color = draw.Red
-	// }
-
-	// window.FillEllipse(centerX-20, centerY-20, 40, 40, color)
-	// window.DrawEllipse(centerX-20, centerY-20, 40, 40, draw.White)
-	// if mouseInCircle {
-	// 	window.DrawScaledText("Close!", centerX-40, centerY+25, 1.6, draw.Green)
-	// }
-
-	// // check all mouse clicks that happened during this frame
-	// for _, click := range window.Clicks() {
-	// 	dx, dy := click.X-centerX, click.Y-centerY
-	// 	squareDist := dx*dx + dy*dy
-	// 	if squareDist <= 20*20 {
-	// 		// close the window and end the application
-	// 		window.Close()
-	// 	}
-	// }
-
+func handleMouse(window draw.Window) {
 	// check all mouse clicks that happened during this frame
 	rand.Seed(time.Now().UnixNano())
 	if !isMouseDown {
@@ -112,7 +88,18 @@ func update(window draw.Window) {
 
 		createTestPlanet(float64(mouseDownPos.X), float64(mouseDownPos.Y), xVel, yVel, mass, &window)
 	}
+}
 
+func handleKeyboard(window draw.Window) {
+	if window.IsKeyDown(draw.KeyEscape) {
+		for _, gameObj := range ggo.GameObjects {
+			ggo.MarkForDeletion(gameObj)
+		}
+	}
+
+	if window.IsKeyDown(draw.KeyQ) {
+		window.Close()
+	}
 }
 
 func runGGOStarts() {
